c00f4fec1ee61e83930a120091bca8e35b281552
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-06-08 20:42:07 +0800

    Update documentation for add_publication function to clarify parameters and functionality

--- R/add_publication.R
+++ R/add_publication.R
@@ -1,10 +1,19 @@
-#' Title
+#' Add a publication to a YAML datastore
 #'
-#' @param doi
-#' @param corresponding
-#' @param file
+#' Retrieve bibliographic metadata for a DOI and append it to a YAML file
+#' of publications. The function fetches a bibentry via rcrossref, annotates
+#' it with a logical `corresponding` field to indicate if one is the
+#' corresponding author, and then writes the combined list to
+#' disk (creating the file if it does not exist).
 #'
-#' @returns
+#' @param doi Character. DOI of the publication (e.g. \"10.1111/ele.12414\").
+#' @param corresponding Logical. Whether to mark the publication as being a
+#'   corresponding author. Defaults to \code{FALSE}.
+#' @param file Character. Path to the YAML file used to store publications.
+#'   If the file does not exist it will be created. Defaults to \"data/publications.yaml\".
+#'
+#' @return Invisibly \code{NULL}. This function is used for its side effect of
+#'   updating the YAML file on disk.
 #'
 #' @export
 #' @examples
