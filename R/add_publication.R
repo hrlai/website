2c1baf0742918ec959938c4d27153b6e7efa9b81
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-06-11 07:31:01 +0800

    Add joint_first parameter to publication function and update YAML entries

--- R/add_publication.R
+++ R/add_publication.R
@@ -9,6 +9,8 @@
 #' @param doi Character. DOI of the publication (e.g. \"10.1111/ele.12414\").
 #' @param corresponding Logical. Whether to mark the publication as being a
 #'   corresponding author. Defaults to \code{FALSE}.
+#' @param joint_first Logical. Whether to mark the publication as having joint
+#'   first authorship. Defaults to \code{FALSE}.
 #' @param file Character. Path to the YAML file used to store publications.
 #'   If the file does not exist it will be created. Defaults to \"data/publications.yaml\".
 #'
@@ -18,18 +20,19 @@
 #' @export
 #' @examples
 #' box::use(R/add_publication[add_publication])
-#' add_publication("10.1111/ele.12414", corresponding = FALSE)
+#' add_publication("10.1111/ele.12414")
 
 add_publication <- function(
   doi,
   corresponding = FALSE,
+  joint_first = FALSE,
   file = "data/publications.yaml"
 ) {
   # download bibligraphy from DOI and then append corresponding
-  # authorship status
+  # and joint first authorship status
   bib_new <-
     rcrossref::cr_cn(doi, format = "bibentry") |>
-    purrr::list_merge(corresponding = corresponding)
+    purrr::list_merge(corresponding = corresponding, joint_first = joint_first)
 
   # read the publication metadata list if it exists
   existing <- if (file.exists(file)) {
